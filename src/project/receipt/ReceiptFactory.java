3816007cf8d036ac813330fbe74fb854a609b656
sam <sam@SamLaptop>
2018-05-02 16:13:46 -0400

    added file config parsing to receiptfactory

--- src/project/receipt/ReceiptFactory.java
+++ src/project/receipt/ReceiptFactory.java
@@ -18,24 +18,42 @@ public class ReceiptFactory {
 	// constructor
 	// 1. Populates array of TaxComputationMethod objects and array of AddOn objects (as if downloaded from the BestBuy web site).
 	// 2. Reads config file to create and save StoreHeader object (store_num, street_addr, etc.) to be used on all receipts.
+
+		ArrayList<String> storeInfo = new ArrayList<String>();
 		File storeConfig = new File("examplestore.txt");
 		try {
-			FileReader reader = new FileReader(storeConfig);
-		} catch (FileNotFoundException e) {
+			
+			BufferedReader br = new BufferedReader(new FileReader(storeConfig));
+			String line;
+		    while ((line = br.readLine()) != null) {
+		       storeInfo.add(line);
+		    }
+		    
+		} catch (Exception e) {
 			//auto-generated
 			e.printStackTrace();
-		}
+		} 
+		
+		//done with file reading, parse lines into properties
+		String streetAddress = storeInfo.get(0);
+		String state = storeInfo.get(1);
+		String zip = storeInfo.get(2);
+		String year = storeInfo.get(3);
+		String phone = storeInfo.get(4);
 		
 	// 3. Based on the state code (e.g., “MD”) creates and stores appropriate StateComputation object to be used on all receipts.
 	}
 
-	public getReceipt(PurchasedItems items, Date date) {
+	public Receipt getReceipt(PurchasedItems items, Date date) {
 	// 1. Sets the current date of the BasicReceipt.
 	// 2. Sets StoreHeader object of the BasicReceipt (by call to SetStoreHeader of BasicReceipt)
 	// 3. Sets the TaxComputationMethod object of the BasicReceipt (by call to the setTaxComputationMethod of BasicReceipt).
 	// 4. Traverses over all AddOn objects, calling the applies method of each. If an AddOn object applies, then determines if the AddOn is of type SecondaryHeader, Rebate, or Coupon.If of type SecondaryHeader, then creates a PreDecorator for othe AddOn. If of type Rebate orCoupon, then creates a PostDecorator.
 	// 5. Links in the decorator object based on the Decorator design pattern.
 	// 6. Returns decorated BasicReceipt object as type Receipt.
+	
+		//TODO: implement
+		return null;
 	}
 	
 }
