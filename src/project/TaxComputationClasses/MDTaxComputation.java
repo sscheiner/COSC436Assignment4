e33c4e24ed117700567764042ef29ed6fdccd4c0
sscheiner <sf.scheiner@gmail.com>
2018-05-06 23:54:08 -0400

    --

--- src/project/TaxComputationClasses/MDTaxComputation.java
+++ src/project/TaxComputationClasses/MDTaxComputation.java
@@ -1,4 +1,5 @@
 package project.TaxComputationClasses;
+import java.util.ArrayList;
 import java.util.Date;
 
 import project.items.*;
@@ -14,10 +15,17 @@ public class MDTaxComputation extends TaxComputationMethod {
 	public double computeTax(PurchasedItems items, ReceiptDate date) {
 		// calls private method taxHoliday as part of this computation
 		//TODO: implement
-		
+		double totalSalesTax = 0.0;
 		//if it is a tax holiday, tax is 0
 		if(taxHoliday(date)) {
 			return 0.0;
+		}
+		else {
+			ArrayList<StoreItem >itemsList = items.getItems();
+			for(StoreItem x : itemsList){
+				double itemPrice = Double.parseDouble(x.getItemPrice());
+				totalSalesTax += 0.06 * itemPrice;
+			}
 		}
 		
 		
